1aafd43ca77217634b0b908052653a26eeceb736
devsylva <sylvaejike@gmail.com>
2023-09-18 12:31:50 +0100

    staging apps

--- stockpulse/settings.py
+++ stockpulse/settings.py
@@ -190,6 +190,10 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+
+POLYGON_API_KEY = env("POLYGON_API_KEY")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
